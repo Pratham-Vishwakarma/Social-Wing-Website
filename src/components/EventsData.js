d0692053967527813e24997f59834de94403121d
Pratham-Vishwakarma <147902380+Pratham-Vishwakarma@users.noreply.github.com>
2025-01-30 12:36:54 +0530

    Updated Event Description

--- src/components/EventsData.js
+++ src/components/EventsData.js
@@ -1,80 +1,60 @@
 import Img1 from '../Images/Events/Diwali.jpg';
 import Img2 from '../Images/Events/ganpati.jpg';
-import Img3 from '../Images/Events/holi celebration.jpg';
-import Img4 from '../Images/Events/tree plantation.jpg';
-import Img5 from '../Images/Events/rakshabandhan.JPG';
-import Img6 from '../Images/Events/chrismas.jpg';
-import Img7 from '../Images/Events/republic day.JPG';
-import Img8 from '../Images/Events/beach clean up .jpg';
-import Img9 from '../Images/Events/stationard donat.jpg';
-import Img10 from '../Images/Events/blood donation.jpg';
-import Img11 from '../Images/Events/flood donation.jpeg';
-import Img12 from '../Images/Events/orhanage.jpg';
-import Img13 from '../Images/Events/martyrs day.jpg';
-import Img14 from '../Images/Events/street-play.JPG';
-import Img15 from '../Images/Events/NSS.jpg';
+import Img3 from '../Images/Events/tree plantation.jpg';
+import Img4 from '../Images/Events/rakshabandhan.JPG';
+import Img5 from '../Images/Events/chrismas.jpg';
+import Img6 from '../Images/Events/republic day.JPG';
+import Img7 from '../Images/Events/beach clean up .jpg';
+import Img8 from '../Images/Events/stationard donat.jpg';
+import Img9 from '../Images/Events/blood donation.jpg';
+import Img10 from '../Images/Events/martyrs day.jpg';
+import Img11 from '../Images/Events/NSS.jpg';
 
 const EventsData = [
     {
         title:"DIWALI CELEBRATION",
-        info: "Description",
+        info: "NSS-RAIT celebrated Diwali by visiting Ashray Foundation Sanstha (Old Age Home), bringing joy to the elderly residents. The day was filled with music, laughter, and heartfelt conversations, creating cherished bonds. A lively game of Antakshari had everyone singing along, reliving old melodies. A special cake-cutting ceremony added to the warmth, symbolizing the beautiful connection formed. The celebration ended with smiles and a deep sense of community, reminding us that Diwali is about love and togetherness.",
         img: Img1
     },{
         title:"GANPATI CELEBRATION",
-        info: "Description",
+        info: "Members from Social Wing-RAIT and NSS volunteers brought 98 underprivileged kids from the Children of  the World NGO to the college. Throughout the journey, the kids had fun sessions of Antakshari. . At the  college, interaction with the kids was followed by taking them for the Aarti of ‘Rait Cha Raja’. As prasad,  we provided them with the ‘Mahaprasad’ of Ganpati which the kids loved. A fun movie session was organized ",
         img: Img2
     },{
-        title:"HOLI CELEBRATION",
-        info: "Description",
+        title:"TREE-PLANTATION DRIVE",
+        info: "On 29th August 2024, a tree plantation drive was held in Panvel, with students and NSS volunteers actively participating. Tools were provided to the participants, who planted saplings in two areas, promoting environmental conservation. The event also featured a tree planting drive on the DY Patil campus, where students, faculty, and staff planted native species, enhancing campus beautification and supporting biodiversity.",
         img: Img3
     },{
-        title:"TREE-PLANTATION DRIVE",
-        info: "Description",
+        title:"RAKSHABANDHAN CELEBRATION",
+        info: "The Social Wing-RAIT and NSS RAIT organized a Raksha Bandhan celebration at the Nerul Police Station, where female volunteers expressed gratitude to the police officers by tying rakhis as a symbol of protection. The event emphasized the significance of community bonds and recognized the police’s dedication to safeguarding society.",
         img: Img4
     },{
-        title:"RAKSHABANDHAN CELEBRATION",
-        info: "Description",
+        title:"CHRISTMAS CELEBRATION",
+        info: "Christmas Food Donation Event to bring joy and sustenance to those in need during the festive season. The event aimed to spread holiday cheer by providing nutritious meals to individuals and families facing food insecurity. It highlighted the true spirit of compassion, kindness, and community, offering both nourishment and a sense of togetherness that will be cherished long after the season.",
         img: Img5
     },{
-        title:"CHRISTMAS CELEBRATION",
-        info: "Description",
+        title:"REPUBLIC DAY CELEBRATION",
+        info: "The event concluded with a collective pledge to uphold the values of the Constitution and contribute positively to society. With active participation from volunteers and the enthusiasm of new NSS aspirants, the webinar instilled a strong sense of patriotism and responsibility among attendees. It served as a meaningful tribute to Republic Day, highlighting the importance of civic duty and social change.",
         img: Img6
     },{
-        title:"REPUBLIC DAY CELEBRATION",
-        info: "Description",
+        title:"CLEAN-UP DRIVE",
+        info: "On 2nd October 2024, a successful cleanup drive was held at Nerul Station in celebration of Gandhi Jayanti. Volunteers, students, and local residents actively participated in collecting and disposing of garbage. The event also featured an awareness session on waste segregation and the importance of reducing plastic waste",
         img: Img7
     },{
-        title:"CLEAN-UP DRIVE",
-        info: "Description",
+        title:"STATIONARY DONATION DRIVE",
+        info: "After formal introductions, the volunteers conducted a career guidance session, offering insights into various career paths and higher education opportunities after 12th grade. The session aimed to inspire students to pursue their dreams despite challenges. Following the discussion, the volunteers distributed stationery kits, symbolizing a shared commitment to education. The event also included interactive games and casual conversations, helping to build rapport and foster a positive connection with the students.",
         img: Img8
     },{
-        title:"STATIONARY DONATION DRIVE",
-        info: "Description",
+        title:"BLOOD DONATION DRIVE",
+        info: "The blood donation event, organized by the Social Wing and NSS volunteers at the college, saw active participation from students, faculty, staff, and people from outside the college. With efficient registration and smooth donation processes, the event successfully addressed the urgent need for blood donations while fostering a sense of community responsibility and altruism.",
         img: Img9
     },{
-        title:"BLOOD DONATION DRIVE",
-        info: "Description",
+        title:"MARTYR'S DAY",
+        info: "The Social Wing of RAIT organized a solemn event to commemorate Martyrs Day. Volunteers created meaningful displays near tribute sites, encouraging active participation in remembrance activities. A heartfelt pledge was taken, with the central aim of instilling respect and patriotism among students, honoring the sacrifices of martyrs, and inspiring positive contributions to society.",
         img: Img10
     },{
-        title:"FLOOD RELIEF DONATION DRIVE",
-        info: "Description",
-        img: Img11
-    },{
-        title:"ORPHANAGE VISIT",
-        info: "Description",
-        img: Img12
-    },{
-        title:"MARTYR'S DAY",
-        info: "Description",
-        img: Img13
-    },{
-        title:"AWARENESS CAMPAIGNS",
-        info: "Description",
-        img: Img14
-    },{
         title:"NSS CAMP",
-        info: "Description",
-        img: Img15
+        info: "The rally aimed to inform villagers about the upcoming camp and planned activities, while also educating them on important social issues such as tree planting, waste management, voting, girl education, and tobacco cessation. Volunteers actively participated, carrying signs and reciting slogans to engage the community.",
+        img: Img11
     },
 ];
 
